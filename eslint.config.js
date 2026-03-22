a3cad0c81e97dce443840ea392d89c4524478732
Nielless Acharya <chiku0210@gmail.com>
2026-03-23 01:53:48 +0530

    feat: update environment variables, migrate to ES module syntax, and enhance error handling

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-const js = require('@eslint/js');
-const tseslint = require('typescript-eslint');
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-module.exports = tseslint.config(
+export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
